3c85d617294388c22ac76bab801f386273961377
ECRAYON | Diego Flores <diego.flores@ecrayon.com.br>
2013-01-22 16:15:23 -0200

    languageTranslator Class :: Controller schema

--- lib/languageTranslation/Controller.php
+++ lib/languageTranslation/Controller.php
@@ -1,8 +1,11 @@
 <?php
 class languageTranslation_Controller extends Controller {
+	protected $objModel;
 	
 	public function __construct() {
+		parent::__construct();
 		
+		$this->objModel = new languageTranslator_Model();
 	}
 	
 	public function setTranslationVars($objData) {

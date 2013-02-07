a6ed6f28b1806c27497d44cfaed7efd52b07be6b
ECRAYON | Diego Flores <diego.flores@ecrayon.com.br>
2013-02-07 18:45:18 -0200

    Fixes Applr::isLoggedIn() method and applies on controller/Config.php

---
+++ cms/controller/Login.php
@@ -0,0 +1,33 @@
+<?php
+class Login_controller extends Main_controller {
+	private $objAuth;
+	
+	public function __construct($boolRenderTemplate = true) {
+		parent::__construct($boolRenderTemplate);
+		
+		$this->objAuth = new authUser_Controller();
+	}
+	
+	/**
+	 * Authenticates user
+	 *
+	 * @param 		string	$strLogin		Username
+	 * @param 		string	$strPwd			Password
+	 *
+	 * @return 		boolean
+	 *
+	 * @since 		2013-01-22
+	 * @author 		Diego Flores <diegotf [at] gmail [dot] com>
+	 *
+	 */
+	public function auth($strLogin,$strPwd) {
+		if($this->objAuth->authUser($strLogin,$strPwd)) {
+			header("Location: " . HTTP_CMS . "config");
+			exit();
+		} else {
+			$this->objSmarty->assign('ERROR_MSG','There was an error in authentication! Please try again!');
+			$this->renderTemplate();
+		}
+	}
+}
+?>

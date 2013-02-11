72d136770a91a8fb50b2ffdddc7181adfa34ec97
ECRAYON | Diego Flores <diego.flores@ecrayon.com.br>
2013-02-11 19:05:24 -0200

    Applr::Media v1

--- cms/controller/Media.php
+++ cms/controller/Media.php
@@ -80,20 +80,24 @@ class Media_controller extends CRUD_controller {
 			default:
 				// Setups MEDIA GALLERY id and dir_path
 				$arrGall	= (!empty($_POST['media_gallery_id']) ? explode('|',$_POST['media_gallery_id']) : array(1,DIR_IMAGE . 'common'));
+				$strFilter	= 'IMAGE';
 			break;
 		
 				// VIDEO
 			case 1:
 				// Setups MEDIA GALLERY id and dir_path
 				$arrGall	= (!empty($_POST['media_gallery_id']) ? explode('|',$_POST['media_gallery_id']) : array(1,DIR_VIDEO . 'common'));
+				$strFilter	= 'VIDEO';
 			break;
 		
 				// UPLOAD
 			case 0:
 				// Setups MEDIA GALLERY id and dir_path
 				$arrGall	= (!empty($_POST['media_gallery_id']) ? explode('|',$_POST['media_gallery_id']) : array(1,DIR_UPLOAD . 'common'));
+				$strFilter	= '';
 			break;
 		}
+		$_POST['media_gallery_id'] = $arrGall[0];
 		
 		// Instantiates uploadFile class
 		$objUpload = new uploadFile_Controller('applr_tmp',SYS_ROOT . $arrGall[1]);
@@ -103,37 +107,74 @@ class Media_controller extends CRUD_controller {
 			$_POST['filepath'] = $_POST['file_url'];
 		} else {
 			$objUpload->setFile('file_upload');
+			$objUpload->setFilter($strFilter);
 			$_POST['filepath'] = $objUpload->uploadFile();
+			if($_POST['filepath'] === false) $_POST['filepath'] = null;
 		}
 		
-		// Sets STREAMING FILE URL or uploads user file
-		if(!empty($_POST['streaming_url'])) {
-			$_POST['filepath_streaming'] = $_POST['streaming_url'];
-		} else {
-			$objUpload->setFile('streaming_upload');
-			$_POST['filepath_streaming'] = $objUpload->uploadFile();
+		if(!empty($_POST['filepath'])) {
+			// Sets STREAMING FILE URL or uploads user file
+			if(!empty($_POST['streaming_url'])) {
+				$_POST['filepath_streaming'] = $_POST['streaming_url'];
+			} elseif($objUpload->setFile('streaming_upload')) {
+				$_POST['filepath_streaming'] = $objUpload->uploadFile();
+				if($_POST['filepath_streaming'] === false) $_POST['filepath_streaming'] = null;
+			}
+			
+			// Sets THUMBNAIL FILE URL or uploads user file
+			$boolUDThumb = false;
+			if(!empty($_POST['thumb_url'])) {
+				$boolUDThumb = true;
+				$_POST['filepath_thumbnail'] = $_POST['thumb_url'];
+			} elseif($objUpload->setFile('thumb_upload')) {
+				$boolUDThumb = true;
+				$objUpload->setFilter('IMAGE');
+				$_POST['filepath_thumbnail'] = $objUpload->uploadFile();
+				if($_POST['filepath_thumbnail'] === false) $_POST['filepath_thumbnail'] = null;
+			}
+			
+			// Sets sys vars
+			if(!isset($_POST['media_gallery_id']) || empty($_POST['media_gallery_id'])) {
+				$_POST['media_gallery_id'] = 1;
+			} elseif(!is_numeric($_POST['media_gallery_id']) || $_POST['media_gallery_id'] <= 0) $_POST['media_gallery_id'] = 1;
+			if(!is_numeric($_POST['mediatype']) || $_POST['mediatype'] < 0 || $_POST['mediatype'] > 2) $_POST['mediatype'] = 2;
+			
+			// Creates autothumb
+			$arrAutoThumb = (array) $this->objModel->select(array('autothumb_h','autothumb_w'),'media_gallery',array(),array('media_gallery.autothumb = 1','media_gallery.id = ' . $_POST['media_gallery_id']),array(),array(),0,null,'Row');
+			if(isset($arrAutoThumb['autothumb_h']) && isset($arrAutoThumb['autothumb_w']) && !empty($_POST['filepath'])) {
+				// Defines original image content and basename
+				// If THUMBNAIL is user defined file
+				if($boolUDThumb) {
+					$strFileName	= basename($_POST['filepath_thumbnail']);
+					$strOrgPath		= (empty($_POST['thumb_url']) ? SYS_ROOT : '') . $_POST['filepath_thumbnail'];
+					$strDestPath	= SYS_ROOT . $arrGall[1] . '/'. $strFileName;
+				} else {
+					$strFileName	= basename($_POST['filepath']);
+					$strOrgPath		= (empty($_POST['file_url']) ? SYS_ROOT : '') . $_POST['filepath'];
+					$strDestPath	= SYS_ROOT . $arrGall[1] . '/'. $strFileName;
+				}
+				
+				$boolCreate		= true;
+				if(!file_exists($strDestPath)) {
+					// Creates original file
+					$strFileData	= file_get_contents($strOrgPath);
+					if(!file_put_contents($strDestPath,$strFileData,LOCK_EX)) {
+						$strDestPath 	= $strOrgPath;
+						$boolCreate		= false;
+					}
+				}
+				
+				// Creates new file, resized
+				if($boolCreate) {
+					$strThumbPath = $objUpload->createThumbnail($strDestPath,$arrAutoThumb['autothumb_w'],$arrAutoThumb['autothumb_h']);
+					@unlink($strDestPath);
+					$strDestPath = $strThumbPath;
+				}
+				
+				// Sets variable to INSERT / UPDATE
+				$_POST['filepath_thumbnail'] = (!empty($strDestPath) ? str_replace(SYS_ROOT,'',$strDestPath) : $_POST['filepath']);
+			}
 		}
-		
-		// Sets THUMBNAIL FILE URL or uploads user file
-		if(!empty($_POST['thumb_url'])) {
-			$_POST['filepath_thumbnail'] = $_POST['thumb_url'];
-		} else {
-			$objUpload->setFile('thumb_upload');
-			$_POST['filepath_thumbnail'] = $objUpload->uploadFile();
-		}
-		
-		// Sets sys vars
-		if(!isset($_POST['media_gallery_id']) || empty($_POST['media_gallery_id'])) {
-			$_POST['media_gallery_id'] = 1;
-		} elseif(!is_numeric($_POST['media_gallery_id']) || $_POST['media_gallery_id'] <= 0) $_POST['media_gallery_id'] = 1;
-		if(!is_numeric($_POST['mediatype']) || $_POST['mediatype'] < 0 || $_POST['mediatype'] > 2) $_POST['mediatype'] = 2;
-		
-		/*
-		
-		if(!isset($_POST['autothumb_h']) || empty($_POST['autothumb_h'])) 	$_POST['autothumb_h'] = null;
-		if(!isset($_POST['autothumb_w']) || empty($_POST['autothumb_w'])) 	$_POST['autothumb_w'] = null;
-		if(!is_null($_POST['autothumb_h']) || !is_null($_POST['autothumb_w'])) $_POST['autothumb'] = 1; else $_POST['autothumb'] = 0; 
-		*/
 		
 		$this->_update($_POST);
 		
